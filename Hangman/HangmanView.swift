633df2bc6041ff137483e2a8885895aba0ec74a2
Alex Banh <alex.banh1881@gmail.com>
2016-09-07 14:12:45 -0700

    Added code comments
    Added comments to ViewController.swift, HangmanView.swift, and
    HangmanBrain.swift

--- Hangman/HangmanView.swift
+++ Hangman/HangmanView.swift
@@ -5,39 +5,50 @@
 //  Created by Alex Banh on 8/8/16.
 //  Copyright © 2016 Flybu. All rights reserved.
 //
+//  HangmanView.swift provides code for the paths and drawing of the gallows image and the 
+//  hangman image. HangmanView is called by ViewController.swift in order to update the hangman 
+//  image accordingly for every guess.
 
 import UIKit
 
+//  Code to render live preview of drawings in Storyboard
 @IBDesignable
 
 class HangmanView: UIView {
     
+    //  Represents the number of wrong guesses. Can also check to see if value was changed externally
     @IBInspectable var wrongs: Int = 0 {
         didSet {
             if wrongs <= 6 {
-                setNeedsDisplay()
+                setNeedsDisplay()           //  Don't exactly understand this code yet
             }
         }
     }
-        
+    
+    //  Represents the scale of the drawings. 1.00 and 0.00 are 100% and 0%, respectively
     var scale: CGFloat = 1.00
     
+    //  Represents a relative unit of width to be used
     var relWidth: CGFloat {
         return (bounds.width/8) * scale
     }
     
+    //  Represents a relative unit of height to be used
     var relHeight: CGFloat {
         return (bounds.height/8) * scale
     }
-        
+    
+    //  Represents a value for the radius of the skull
     var skullRadius: CGFloat {
         return relHeight / 2
     }
-        
+    
+    //  A point which represents the center point for the hangman's head
     var skullCenter: CGPoint {
         return CGPoint(x: relWidth * 6, y: (relHeight * 3) + skullRadius)
     }
     
+    //  returns a path representing the path to follow for drawing the gallows
     private func pathForGallows() -> UIBezierPath {
         let gallowPath = UIBezierPath()
         gallowPath.moveToPoint(CGPoint(x:relWidth,y:(relHeight)*7))
@@ -50,11 +61,13 @@ class HangmanView: UIView {
         return gallowPath
     }
     
+    //  returns a path representing the path to follow for drawing the head
     private func pathForHead() -> UIBezierPath {
         let headPath = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2 * M_PI), clockwise: false)
         return headPath
     }
     
+    //  returns a path representing the path to follow for drawing the body
     private func pathForBody() -> UIBezierPath {
         let bodyPath = UIBezierPath()
         bodyPath.moveToPoint(CGPoint(x: relWidth * 6, y: relHeight * 4))
@@ -62,6 +75,7 @@ class HangmanView: UIView {
         return bodyPath
     }
     
+    //  returns a path representing the path to follow for drawing a leg
     private func pathForLeg(x_value: CGFloat) -> UIBezierPath {
         let legPath = UIBezierPath()
         legPath.moveToPoint(CGPoint(x: relWidth * 6, y: relHeight * 5.5))
@@ -69,6 +83,7 @@ class HangmanView: UIView {
         return legPath
     }
     
+    //  returns a path representing the path to follow for drawing an arm
     private func pathForArm(x_value: CGFloat) -> UIBezierPath {
         let armPath = UIBezierPath()
         armPath.moveToPoint(CGPoint(x: relWidth * 6, y: relHeight * 4.5))
@@ -76,6 +91,7 @@ class HangmanView: UIView {
         return armPath
     }
 
+    //  draws various hangman paths depending on the number of guesses which have been used
     override func drawRect(rect: CGRect) {
         pathForGallows().stroke()
         if (wrongs >= 1) {
